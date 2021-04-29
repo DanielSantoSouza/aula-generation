4b08c353aa6bba5067f802183cd4859fc76a82c3
danielqjc <danielsantos304@ymail.com>
2021-04-28 23:01:02 -0300

    exercicios

---
+++ blogpessoal/src/main/java/snippet/Snippet.java
@@ -0,0 +1,7 @@
+package snippet;
+
+public class Snippet {
+	INSERT INTO `blog_pessoal`.`^postagem` (`data`,`texto`,`titulo`)
+	       VALUES (`2020-04-30 10:10:10.000`,`estou aprendendo`,`API Rest Spring`);
+}
+
