339b2e36728dadb3cad09e2fdf187952b6cb172c
danielqjc <danielsantos304@ymail.com>
2021-05-10 21:54:26 -0300

    atualização do blogpessoal

--- blogpessoal/src/main/java/snippet/Snippet.java
+++
@@ -1,7 +0,0 @@
-package snippet;
-
-public class Snippet {
-	INSERT INTO `blog_pessoal`.`^postagem` (`data`,`texto`,`titulo`)
-	       VALUES (`2020-04-30 10:10:10.000`,`estou aprendendo`,`API Rest Spring`);
-}
-
